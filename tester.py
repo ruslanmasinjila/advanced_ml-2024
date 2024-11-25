3be0b96750439350880efc52eeb72d5e904c96f0
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2024-11-25 01:35:58 -0330

    about to convert elements of augmneted_sensor_data to polygon

--- tester.py
+++ tester.py
@@ -7,16 +7,12 @@ augmneted_sensor_data = None
 with open('augmented_sensor_data.pkl', 'rb') as f:
     augmneted_sensor_data = pickle.load(f)
 
-x = augmneted_sensor_data[500]['x']
-y = augmneted_sensor_data[500]['y']
 
-# Plot
-plt.figure(figsize=(8, 8))
-plt.scatter(x, y, color='blue', s=10, label='Data points')
-plt.gca().set_aspect('equal', adjustable='box')
-plt.title('Generated Data Points')
-plt.xlabel('X')
-plt.ylabel('Y')
-plt.grid(True)
-plt.legend()
-plt.show()
+
+l = len(augmneted_sensor_data)
+print(l)
+shape_a = augmneted_sensor_data[:int(l/2)]
+shape_b = augmneted_sensor_data[int(l/2):]
+
+print(len(shape_a))
+print(len(shape_b))
