646854ffb794625a8ec80c7a69d4af359b6c4778
Ruslan Masinjila <ruslanmasinjila@gmail.com>
2024-12-02 22:15:46 -0330

    updated README.md

--- 4 GAM.py
+++ 4 GAM.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
-from pygam import GAM, s, LinearGAM
+from pygam import GAM, s
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.model_selection import cross_val_score, KFold
 from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
@@ -60,7 +60,7 @@ for train_index, test_index in kf.split(X):
     y_train, y_test = y_x[train_index], y_x[test_index]
     
     # Initialize the Generalized Additive Model
-    gam = LinearGAM(s(0) + s(1) + s(2) + s(3))  # Using smooth terms for each feature
+    gam = GAM(s(0) + s(1) + s(2) + s(3))  # Using smooth terms for each feature
     
     # Fit the model on the training data
     gam.fit(X_train, y_train)
@@ -78,12 +78,14 @@ for train_index, test_index in kf.split(X):
 end_timeX = time.time()
 
 # Calculate the average mse across all folds
-average_mse = sum(mse_scores) / len(mse_scores)
+average_mse_y_x =   np.mean(mse_scores)
+std_y_x         =   np.std(mse_scores)
 
-print(f'Average mse across 5 folds on y_x: {average_mse}')
+print(f'Average mse across 5 folds on y_x: {average_mse_y_x}')
+print(f'Standard Deviation in mse across 5 folds on y_x: {std_y_x}')
 
 # Initialize the Generalized Additive Model
-gamYx = LinearGAM(s(0) + s(1) + s(2) + s(3))  # Using smooth terms for each feature
+gamYx = GAM(s(0) + s(1) + s(2) + s(3))  # Using smooth terms for each feature
 
 # Train the model on the entire dataset
 gamYx.fit(X, y_x)
@@ -111,7 +113,7 @@ for train_index, test_index in kf.split(X):
     y_train, y_test = y_y[train_index], y_y[test_index]
     
     # Initialize the Generalized Additive Model
-    gam = LinearGAM(s(0) + s(1) + s(2) + s(3))  # Using smooth terms for each feature
+    gam = GAM(s(0) + s(1) + s(2) + s(3))  # Using smooth terms for each feature
     
     # Fit the model on the training data
     gam.fit(X_train, y_train)
@@ -129,10 +131,11 @@ for train_index, test_index in kf.split(X):
 end_timeY = time.time()
 
 # Calculate the average mse across all folds
-average_mse = sum(mse_scores) / len(mse_scores)
+average_mse_y_y =   np.mean(mse_scores)
+std_y_y         =   np.std(mse_scores)
 
-print(f'Average mse across 5 folds on y_y: {average_mse}')
-print(f'Total Time: {(end_timeX-start_timeX)+(end_timeY-start_timeY)}')
+print(f'Average mse across 5 folds on y_y: {average_mse_y_y}')
+print(f'Standard Deviation in mse across 5 folds on y_y: {std_y_y}')
 
 # Initialize the Generalized Additive Model
 gamYy = GAM(s(0) + s(1) + s(2) + s(3))  # Using smooth terms for each feature
@@ -146,3 +149,94 @@ with open('gamYy.pkl', 'wb') as f:
 
 
 ###############################################################################################
+# INFERENCE
+###############################################################################################
+
+
+gamYx = None
+gamYy = None
+
+# Load the saved model using pickle
+with open('gamYx.pkl', 'rb') as f:
+    gamYx = pickle.load(f)
+
+# Load the saved model using pickle
+with open('gamYy.pkl', 'rb') as f:
+    gamYy = pickle.load(f)
+
+list_shape_a = None
+list_shape_b = None
+list_shape_result = None
+
+# Load data from pickle files
+with open('list_shape_a.pkl', 'rb') as f:
+    list_shape_a = pickle.load(f)
+
+
+with open('list_shape_b.pkl', 'rb') as f:
+    list_shape_b = pickle.load(f)
+
+
+with open('list_shape_result.pkl', 'rb') as f:
+    list_shape_result = pickle.load(f)
+
+list_shape_a = np.array(list_shape_a[-100:])
+list_shape_b = np.array(list_shape_b[-100:])
+list_shape_result = np.array(list_shape_result[-100:])
+
+mse_inference = []
+actual_result = []
+predicted_result = []
+
+
+for i in range(len(list_shape_a)):
+    shape_a = list_shape_a[i].reshape(-1, 2)
+    shape_b = list_shape_b[i].reshape(-1, 2)
+    shape_result = list_shape_result[i].reshape(-1, 2)
+
+    shape_a_df = pd.DataFrame(shape_a, columns=['x_shape_a','y_shape_a'])
+    shape_b_df = pd.DataFrame(shape_b, columns=['x_shape_b','y_shape_b'])
+    shape_result_df = pd.DataFrame(shape_result, columns=['x_shape_result','y_shape_result'])
+
+    joint_df = pd.concat([shape_a_df, shape_b_df, shape_result_df], axis=1)
+    X_new = joint_df[['x_shape_a', 'y_shape_a', 'x_shape_b', 'y_shape_b']].values
+
+    Yx_pred_new = gamYx.predict(X_new)
+    Yy_pred_new = gamYy.predict(X_new)
+
+    pred_df = pd.DataFrame()
+    pred_df['x_shape_result'] = Yx_pred_new
+    pred_df['y_shape_result'] = Yy_pred_new
+
+    for col in pred_df.columns:
+        mse = ((pred_df[col] - shape_result_df[col]) ** 2).mean()
+        mse_inference.append(mse)
+    
+    actual_result.append(shape_result_df)
+    predicted_result.append(pred_df)
+
+print(f'Average mse on 100 inferences: {np.mean(mse_inference)}')
+
+
+'''
+
+index = 30
+
+plt.figure(figsize=(8, 8))
+
+# Plot the first set of points
+plt.scatter(predicted_result[index]['x_shape_result'], predicted_result[index]['y_shape_result'], color='blue', s=10, label='Prediction')
+
+# Add another set of points with different color
+plt.scatter(actual_result[index]['x_shape_result'], actual_result[index]['y_shape_result'], color='red', s=10, label='Actual Result')
+
+# Set equal aspect ratio and other properties
+plt.gca().set_aspect('equal', adjustable='box')
+plt.title('Actual VS Prediction for GAM')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.grid(True)
+plt.legend()
+plt.show()
+
+'''
--- README.md
+++ README.md
@@ -1,3 +1,12 @@
-#<TODO>
+To run the files online, visit:
+<br>
+[Advanced ML Project on Google Colab](https://colab.research.google.com/drive/1ZEeIfScQ4JfP9wJHvYu_SFOq20Q9gTXt?usp=sharing)
+<br>
+To run the files locally, follow the following order:
+- 1 sensor model.py
+- 2 data augmento.py
+- 3 data interpolator.py
+- 4 GAM.py
+- 5 GAN.py
+- 6 CNN.py
 
-- Add Link to Colab
